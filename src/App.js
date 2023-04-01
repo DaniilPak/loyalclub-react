201319b177141660567de0c69789c72b10b367be
DaniilPak <1asumeasume1@gmail.com>
2023-04-01 21:58:45 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState('');
+
+  // Function to authenticate user and get JWT token
+  const handleLogin = async () => {
+    const response = await axios.post('http://localhost:3000/api/auth', { "phoneNumber": "77054809351", "password": "password" });
+    setToken(response.data.token);
+    console.log(token);
+  };
+
+  // Function to access protected data
+  const handleProtectedData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/protected', { headers: { Authorization: token } });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  // Function to access protected data
+  const updateBusinessInfo = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/api/business/update', { 
+        "name": "K.chicken"
+      } , { headers: { Authorization: token, businessid: "6424638f5e0484cbd121f1d8" } });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleProtectedData}>Access Protected Data</button>
+      <button onClick={updateBusinessInfo}>Update business info</button>
     </div>
   );
-}
+};
 
 export default App;
